39f5243fe85709edc1b5e6df2cc7bf61496b90f9
Khurtina Katy <rainbow.ekaterina@gmail.com>
2013-12-30 14:09:39 +0400

    Experiment with adding css rules to element dynamically

--- ColorABC.js
+++ ColorABC.js
@@ -3,10 +3,21 @@ function ColorABC(text) {
 };
 
 ColorABC.prototype.createABC = function(text) {
-    var alphabet_string = "abcdefghijklmnopqrstuvwxyz";
-    var alphabet = alphabet_string.split(",");
-    console.log(typeof alphabet);
+    var alp = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+    var alphabet =alp.split(" ").concat(alp.toUpperCase().split(" "));
+    console.log(alphabet);
+    var sym = "! @ # $ % ^ & * ( ) _ - + = { } ' \"  | / ? < > , . ";
+    var symbols = sym.split(" ");
+    console.log(symbols);
+},
+
+ColorABC.prototype.getColor = function() {
+    return "#" + Math.floor(Math.random() * 0xff);
 }
 
 var colorABC = new ColorABC("text");
 colorABC.createABC("text");
+var c = colorABC.getColor();
+
+var r = document.getElementById("color");
+r.style.background = c;
