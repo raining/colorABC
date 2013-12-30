15e8e938be6b44c9e75efd273f4f780e80018e12
Khurtina Katy <rainbow.ekaterina@gmail.com>
2013-12-30 21:55:53 +0400

    Fixed getColor function; Add ColorArray; try to color text

--- ColorABC.js
+++ ColorABC.js
@@ -2,22 +2,51 @@ function ColorABC(text) {
     this.text = text;
 };
 
-ColorABC.prototype.createABC = function(text) {
+
+ColorABC.prototype.getSymArray = function() {
     var alp = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
     var alphabet =alp.split(" ").concat(alp.toUpperCase().split(" "));
-    console.log(alphabet);
+
     var sym = "! @ # $ % ^ & * ( ) _ - + = { } ' \"  | / ? < > , . ";
     var symbols = sym.split(" ");
-    console.log(symbols);
-},
+
+    var num = "1 2 3 4 5 6 7 8 9 0";
+    var numbers = num.split(" ");
+
+    return alphabet.concat(symbols, numbers);
+};
 
 ColorABC.prototype.getColor = function() {
-    return "#" + Math.floor(Math.random() * 0xff);
-}
+
+    var color = Math.floor((Math.random() * 0xFFFFFF)).toString(16);
+    console.log(color);
+    return '#' + color;
+};
+
+
+ColorABC.SymArray = new ColorABC("").getSymArray();
+ColorABC.ColorArray = new ColorABC("").getColorArray(ColorABC.SymArray);
+
+ColorABC.prototype.getColorArray = function(SymArray) {
+    var colorArray = [];
+    for (var r in SymArray) {
+        var color = new ColorABC("").getColor();
+        colorArray.push(color);
+    }
+};
+
+ColorABC.prototype.createABC = function() {
+    for (var i = 0; i < this.text.length; i++) {
+        var textItem = document.getElementsByTagName("textarea");
+        textItem.style.background = ColorABC.ColorArray[Math.floor(Math.random() * ColorABC.ColorArray.length)];
+    }
+};
+
 
 var colorABC = new ColorABC("text");
-colorABC.createABC("text");
+//colorABC.createABC("text");
 var c = colorABC.getColor();
+colorABC.getColorArray(ColorABC.SymArray);
 
 var r = document.getElementById("color");
 r.style.background = c;
