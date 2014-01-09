311c3efc04e4f3f4216da9e3f818108c252363cd
Khurtina Katy <rainbow.ekaterina@gmail.com>
2014-01-09 14:46:57 +0400

    Add interactive functionality to btns; fixed text getter

--- ColorABC.js
+++ ColorABC.js
@@ -3,50 +3,42 @@ function ColorABC(text) {
 };
 
 
-ColorABC.prototype.getSymArray = function() {
-    var alp = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
-    var alphabet =alp.split(" ").concat(alp.toUpperCase().split(" "));
-
-    var sym = "! @ # $ % ^ & * ( ) _ - + = { } ' \"  | / ? < > , . ";
-    var symbols = sym.split(" ");
-
-    var num = "1 2 3 4 5 6 7 8 9 0";
-    var numbers = num.split(" ");
-
-    return alphabet.concat(symbols, numbers);
-};
+//ColorABC.prototype.getSymArray = function() {
+//    var alp = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+//    var alphabet =alp.split(" ").concat(alp.toUpperCase().split(" "));
+//
+//    var sym = "! @ # $ % ^ & * ( ) _ - + = { } ' \"  | / ? < > , . ";
+//    var symbols = sym.split(" ");
+//
+//    var num = "1 2 3 4 5 6 7 8 9 0";
+//    var numbers = num.split(" ");
+//
+//    return alphabet.concat(symbols, numbers);
+//};
 
 ColorABC.prototype.getColor = function() {
 
     var color = Math.floor((Math.random() * 0xFFFFFF)).toString(16);
-    console.log(color);
     return '#' + color;
 };
 
 
-ColorABC.SymArray = new ColorABC("").getSymArray();
-ColorABC.ColorArray = new ColorABC("").getColorArray(ColorABC.SymArray);
-
-ColorABC.prototype.getColorArray = function(SymArray) {
-    var colorArray = [];
-    for (var r in SymArray) {
-        var color = new ColorABC("").getColor();
-        colorArray.push(color);
-    }
-};
 
 ColorABC.prototype.createABC = function() {
-    for (var i = 0; i < this.text.length; i++) {
-        var textItem = document.getElementsByTagName("textarea");
-        textItem.style.background = ColorABC.ColorArray[Math.floor(Math.random() * ColorABC.ColorArray.length)];
-    }
+
 };
 
+//ColorABC.SumArray = new ColorABC("").getSymArray();
+//ColorABC.ColorArray = new ColorABC("").getColorArray(SumArray);
 
-var colorABC = new ColorABC("text");
-//colorABC.createABC("text");
-var c = colorABC.getColor();
-colorABC.getColorArray(ColorABC.SymArray);
+//console.log("SumArray and ColorArray", SumArray, ColorArray);
 
-var r = document.getElementById("color");
-r.style.background = c;
+
+//var colorABC = new ColorABC("text");
+//colorABC.createABC();
+//
+//var c = colorABC.getColor();
+//console.log(c);
+//
+//var r = document.getElementById("color");
+//r.style.background = c;
